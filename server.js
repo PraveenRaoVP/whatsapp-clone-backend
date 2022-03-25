461f5535346781d39ff054eb85c48f780ca64a69
PraveenRaoVP <praveenraovp01@gmail.com>
2022-03-25 19:09:14 +0530

    first commit, connected mongoose api

---
+++ server.js
@@ -0,0 +1,33 @@
+//import 
+import express from 'express';
+import mongoose from 'mongoose';
+//app config
+const app = express();
+const port = process.env.PORT || 9000;
+
+//middlewares
+
+//??
+
+//DB config
+const connection_url = "mongodb+srv://Praveen_Rao_V_P:whatsappclone@cluster0.w4zdp.mongodb.net/whatsappdb?retryWrites=true&w=majority"
+mongoose.connect(connection_url);
+
+//API routes
+app.get('/',(req,res) => res.status(200).send("Hello World"))
+app.post('/api/v1/messages/new', (req,res) => {
+    const dbMessage = req.body
+
+    Messages.create(dbMessage,(err,data) => {
+        if(err){
+            res.status(500).send(err)
+        }
+        else{
+            res.status(201).send(`New message created /n ${data}`)
+        }
+    })
+})
+
+
+//Listener
+app.listen(port, () => console.log(`Listening in localhost:${port}`))
